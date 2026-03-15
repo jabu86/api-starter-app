341465bb736bd908f31c2542a756640b2b96d6c3
jabu86 <jabumadela86@gmail.com>
2026-03-15 17:17:32 +0200

    commit with web storm

--- api/middleware/roleMiddleware.js
+++ api/middleware/roleMiddleware.js
@@ -3,18 +3,17 @@ const {User, Role} = require('../models');
 const roleMiddleware = (...allowedRoles) => {
   return async (req, res, next) => {
       try {
-          console.log(req.user);
 
           const user = await User.findByPk(req.user.id, {
               include: Role
           });
           const userRoles = user.Roles?.map(role => role.name) || [];
 
-          console.log(userRoles)
+
           const hasRole = allowedRoles.some(role =>
               userRoles.includes(role)
           );
-          console.log(hasRole);
+          
 
           if (!hasRole) {
               return res.status(403).json({
